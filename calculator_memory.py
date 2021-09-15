61457f47ba1f9624851f57f254c22e36ff1971a5
badolin <38982825+badolin@users.noreply.github.com>
2021-09-15 16:39:16 +0200

    Update calculator_memory.py
    add input_index_memory() and input_float_or_Y(text)

--- calculator_memory.py
+++ calculator_memory.py
@@ -1,8 +1,6 @@
 from os import system
 
 class Calculator:
-    memory = None
-    last_calculations = None
     def __init__(self):
         self.memory = []
         self.last_calculations = []
@@ -16,7 +14,11 @@ class Calculator:
 
     @staticmethod
     def multiply(value1_multiply, value2_multiply):
-        return value1_multiply * value2_multiply
+        try:
+            return value1_multiply * value2_multiply
+        except TypeError:
+            print(value1_multiply)
+            print(value2_multiply)
 
     @staticmethod
     def divide(value1_divide, value2_divide):
@@ -28,18 +30,17 @@ class Calculator:
     def print_memory(self):
         system('clear')
         print('Your memory:')
-        i = 1
-        for x in self.memory:
-            print(str(i) + '. ' + str(x))
-            i=i+1
+        for count, item in enumerate(self.memory, 1):
+            print(count, item)
+
     def save_to_memory(self, result):
         self.memory.append(result)
 
     def print_last_calculations(self):
         system('clear')
         print('Last calculations:')
-        for x in self.last_calculations:
-            print(x)
+        for count, item in enumerate(self.last_calculations, 1):
+            print(count, item)
 
     def save_to_last_calculations(self, equation):
         self.last_calculations.append(equation)
@@ -47,23 +48,26 @@ class Calculator:
 
 ########################
 
-def first_number():
-    x = input("Write your first number or write Y when you want use result from memory")
-    if x == 'Y':
-        calculator.print_memory()
+def input_index_memory():
+    calculator.print_memory()
+    try:
         position= int(input("Write which position you want to use"))
         return calculator.memory[position-1]
-    else:
-        return float(x)
+    except:
+        print('You dont write a good number')
+        return None
 
-def second_number():
-    x = input("Write your second number or write Y when you want use result from memory")
+
+def input_float_or_Y(text):
+    x = input(text)
     if x == 'Y':
-        calculator.print_memory()
-        position= int(input("Write which position you want to use"))
-        return calculator.memory[position-1]
+        return input_index_memory()
     else:
-        return float(x)
+        try:
+            return float(x)
+        except:
+            print('You dont write a number')
+            return None
 
 def print_result(result):
     print(result)
@@ -73,8 +77,12 @@ def print_result(result):
 def menu_add():
     system('clear')
     print('You chose addition.')
-    value1_add = first_number()
-    value2_add = second_number()
+    value1_add = None
+    value2_add = None
+    while value1_add == None:
+        value1_add = input_float_or_Y("Write your first number or write Y when you want use result from memory")
+    while value2_add == None:
+        value2_add = input_float_or_Y("Write your second number or write Y when you want use result from memory")
     result = Calculator.add(value1_add, value2_add)
     calculator.save_to_last_calculations(str(value1_add) + '+' + str(value2_add) + '=' + str(result))
     print_result(result)
@@ -82,8 +90,13 @@ def menu_add():
 def menu_subtract():
     system('clear')
     print('You chose subtraction.')
-    value1_subtract = first_number()
-    value2_subtract = second_number()
+    value1_subtract = None
+    value2_subtract = None
+    while value1_subtract == None:
+        value1_subtract = input_float_or_Y("Write your first number or write Y when you want use result from memory")
+    while value2_subtract == None:
+        value2_subtract = input_float_or_Y("Write your second number or write Y when you want use result from memory")
+
     result = Calculator.subtract(value1_subtract, value2_subtract)
     calculator.save_to_last_calculations(str(value1_subtract) + '-' + str(value2_subtract) + '=' + str(result))
     print_result(result)
@@ -91,8 +104,12 @@ def menu_subtract():
 def menu_multiply():
     system('clear')
     print('You chose multiplication.')
-    value1_multiply = first_number()
-    value2_multiply = second_number()
+    value1_multiply = None
+    value2_multiply = None
+    while value1_multiply == None:
+        value1_multiply = input_float_or_Y("Write your first number or write Y when you want use result from memory")
+    while value2_multiply == None:
+        value2_multiply = input_float_or_Y("Write your second number or write Y when you want use result from memory")
     result = Calculator.multiply(value1_multiply, value2_multiply)
     calculator.save_to_last_calculations(str(value1_multiply) + '*' + str(value2_multiply) + '=' + str(result))
     print_result(result)
@@ -100,8 +117,12 @@ def menu_multiply():
 def menu_divide():
     system('clear')
     print('You chose division.')
-    value1_divide = first_number()
-    value2_divide = second_number()
+    value1_divide = None
+    value2_divide = None
+    while value1_divide == None:
+        value1_divide = input_float_or_Y("Write your first number or write Y when you want use result from memory")
+    while value2_divide == None:
+        value2_divide = input_float_or_Y("Write your second number or write Y when you want use result from memory")
     result = Calculator.divide(value1_divide, value2_divide)
     calculator.save_to_last_calculations(str(value1_divide) + '/' + str(value2_divide) + '=' + str(result))
     print_result(result)
@@ -137,13 +158,8 @@ def menu():
 def main():
     print('Welcome to my calculator!')
     while True:
-        try:
-            if menu() == "USER_QUIT":
-                break
-        except ValueError:
-            system('clear')
-            print('You dont write a number')
-            continue
+        if menu() == "USER_QUIT":
+            break
 
 if __name__ == '__main__':
     system('clear')
